d27613cce5a0c4236ad9eca68c3876ac83fb5e77
mac <mac@Rizwan-MacBook-Pro.local>
2022-09-06 17:49:47 +0400

    test

--- server.js
+++ server.js
@@ -1,10 +1,66 @@
 
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//         console.log(req.url, req.method);
+
+
+//         /// set header content
+//         res.setHeader('content-Type','text/html');
+//         res.write('<p>hello</p>');
+//         res.end();
+// });
+
+
+// server.listen(3000,'localhost',() => {
+//         console.log('Listening requests on port 3000');
+// });
+
 const http = require('http');
-const server = http.createServer((req, res) => {
-        console.log('request made');
+const fs = require('fs');
+const _ = require('lodash');
+
+const server = http.createServer((req, res) =>{
+       const Num = _.random(2,20);
+       console.log(Num);
+
+
+        res.setHeader('content-Type','text/html');
+        let path = './views/';
+        switch(req.url){
+                case '/':
+                        path += 'index.html';
+                        res.statusCode = 200;
+                        break;
+                case '/about':
+                        path += 'about.html';
+                        res.statusCode = 200;
+                        break;
+                case '/about-me':
+                        res.statusCode = 301;
+                        res.setHeader('Location','/about');
+                        res.end();
+                        break;        
+                default:
+                        path += '404.html';
+                        res.statusCode = 404;
+                        break;                
+        }
+
+
+
+        fs.readFile(path,(err, data)=>{
+                if(err){
+                        console.log(err);
+                        res.end();
+                }
+                else{
+                        res.write(data);
+                        res.end();
+                }
+        });
 });
 
 
-server.listen(3000,'localhost',() => {
-        console.log('Listening requests on port 3000');
+server.listen('3000','localhost', ()=>{
+                        console.log('Listening require for port 3000');
 });
